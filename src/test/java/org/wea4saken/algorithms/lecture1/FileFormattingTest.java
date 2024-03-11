9b4be71ff7429cc4fa7e3a6467b7b93b6e7a3926
Ilya Kondratyuk <ikondratyuk@systematica-consulting.ru>
2024-03-11 16:25:35 +0700

    Lecture 1 Solve task C. File Formatting

---
+++ src/test/java/org/wea4saken/algorithms/lecture1/FileFormattingTest.java
@@ -0,0 +1,27 @@
+package session1;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FileFormattingTest {
+
+    @Test
+    public void testFileFormatting2() {
+
+        testInput("5\n1\n4\n12\n9\n0\n", "8");
+    }
+
+    private void testInput(String input, String expectedOutput) {
+        System.setIn(new java.io.ByteArrayInputStream(input.getBytes()));
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        FileFormatting.main(null);
+        String actualOutput = outputStream.toString().trim();
+        assertEquals(expectedOutput, actualOutput);
+        System.setIn(System.in);
+    }
+}
